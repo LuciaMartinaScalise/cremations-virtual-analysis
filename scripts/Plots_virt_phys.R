ecc0fddde4e1f09724944a7fa0ff1c0b3fe2c68f
L. Martina Scalise <lms217@cam.ac.uk>
2025-03-15 13:02:31 +0000

    Update Plots_virt_phys.R

--- scripts/Plots_virt_phys.R
+++ scripts/Plots_virt_phys.R
@@ -4,8 +4,10 @@ library(ggplot2)
 library(dplyr)
 library(patchwork)
 virt_phys <- read.csv(here("data","virt_phys.csv"))
+
+# Manually set the factor levels for section
 virt_phys <- virt_phys %>%
-  mutate(section = factor(section, levels = c("upper", "central", "lower")))
+ mutate(section = factor(section, levels = c("upper", "central", "lower","total","half_A","half_B","half_C", "half_D")))
 
 # Customised palettes
 cb_palette <-c("#e14444","darkgrey","navyblue","#E0E0E0")
